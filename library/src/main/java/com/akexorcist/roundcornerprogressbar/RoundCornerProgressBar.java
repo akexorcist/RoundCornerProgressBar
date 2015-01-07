bddce999e63388c28af8bde9aa04f7f25db09085
Akexorcist <akexorcist@gmail.com>
2015-01-07 22:44:59 +0700

    Update RoundCornerProgressBar.java

--- library/src/main/java/com/akexorcist/roundcornerprogressbar/RoundCornerProgressBar.java
+++ library/src/main/java/com/akexorcist/roundcornerprogressbar/RoundCornerProgressBar.java
@@ -1,6 +1,6 @@
 /*
 
-Copyright 2014 Akexorcist
+Copyright 2015 Akexorcist
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
