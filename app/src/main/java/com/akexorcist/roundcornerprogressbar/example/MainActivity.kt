c0470e11ac333f17ee8bbbf150a2cab9b111a591
Akexorcist <akexorcist@gmail.com>
2020-04-19 00:42:54 +0700

    Update layout

--- app/src/main/java/com/akexorcist/roundcornerprogressbar/example/MainActivity.kt
+++ app/src/main/java/com/akexorcist/roundcornerprogressbar/example/MainActivity.kt
@@ -2,15 +2,14 @@ package com.akexorcist.roundcornerprogressbar.example
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
-import kotlinx.android.synthetic.main.activity_main2.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        setContentView(R.layout.activity_main)
 
         val adapter = ViewPagerAdapter(this)
         viewPager.adapter = adapter
