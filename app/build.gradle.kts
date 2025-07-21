b54d590ed3a89f1c48e491c9891f5df1a5b1c339
Akexorcist <akexorcist@gmail.com>
2025-07-22 02:39:04 +0700

    fix: incorrect application ID

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     compileSdk = libs.versions.compileSdk.get().toInt()
 
     defaultConfig {
-        applicationId = "com.akexorcist.roundcornerprogressbar.example"
+        applicationId = "com.akexorcist.roundcornerprogressbar"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
         versionCode = 31
